13c10d37ee7fbac6198b93aff01c716a384536de
shshank01 <shshank980@gmail.com>
2026-08-19 13:32:53 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,39 +1,7 @@
-# write a python program to print the content of a directory using the os module. Search online for the function which does that.
-'''import os
-files = os.listdir()
-print(files)'''
-
-# To print the path of the current working directory
-'''import os
-print(os.getcwd())'''
-
-# To create a new folder in the current directory
-'''import os
-os.mkdir('demo')'''
-
-# To check whether the folder exist or not
-'''import os
-print(os.path.exists('demo'))
-print(os.path.exists('a1'))'''
-
-# To rename the file or directory
-'''import os
-os.rename('2.py','rename.py')'''
-
-# To check whether the argument is file or not
-'''import os
-print(os.path.isfile('3.py'))
-print(os.path.isfile('p.py'))'''
-
-# To check whether the argument is directory or not
-'''import os
-print(os.path.isdir('demo'))
-print(os.path.isdir('hallelujah'))'''
-
-# To remove a file from current directory
-'''import os
-os.remove('3.py')'''
-
-# To remove a file from current directory
-import os
-os.rmdir('demo')
+# write a python program to perform addition, subtraction, multiplication, division of two numbers. also find quotient.
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("error" if a==0 else "Division: ", a/b)
---
+++ 3.py
@@ -0,0 +1,3 @@
+# check the type of variable assigned using input() function
+a=input("Enter: ")
+print("Type: ", type(a))
