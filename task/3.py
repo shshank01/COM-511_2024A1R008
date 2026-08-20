f29c5d93c9e152d7768ee09b23c9e19b56f17654
shshank01 <shshank980@gmail.com>
2026-08-20 12:07:08 +0530

    modifies files

--- task/3.py
+++ task/3.py
@@ -1,3 +1,7 @@
 # check the type of variable assigned using input() function
 a=input("Enter: ")
 print("Type: ", type(a))
+a=int(input("Enter: "))
+print("Type: ", type(a))
+a=float(input("Enter: "))
+print("Type: ", type(a))
--- task/4.py
+++ task/4.py
@@ -1,7 +1,4 @@
 # check whether a is greater than b
 a=int(input("Enter a: "))
 b=int(input("Enter b: "))
-if a>b:
-    print("a is greater than b")
-else:
-    print("a is not greater than b")
+print(a>b)
