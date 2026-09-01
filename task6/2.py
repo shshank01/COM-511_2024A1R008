8256650a43e845f1f1eef130d9528c70f08053bb
shshank01 <shshank980@gmail.com>
2026-09-01 12:02:23 +0530

    c

---
+++ task6/2.py
@@ -0,0 +1,7 @@
+'''Write a python program to fill the given letter tempelate with name and date'''
+name=input("Enter name: ")
+date=input("Enter date: ")
+# print(f"Dear {name},\nYou are selected!\n{date}")
+print(f"""Dear {name},
+You are selected!
+{date}""")
