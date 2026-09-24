befe30cc90aade330220f086a153d5ab06cd7a9d
shshank01 <shshank980@gmail.com>
2026-09-24 12:21:31 +0530

    24 sept:

--- task12/5.py
+++ task12/5.py
@@ -6,7 +6,6 @@ ls=list()
 #         n.remove(i)
 # print("Updated list: ", n)
 # OR
-for i in n:
-    if n.count(i)>=2:
-        n.remove(i)
-print("Updated list: ", n)
+if i not in ls:
+        ls.append(i)
+print("Updated list: ", ls)
